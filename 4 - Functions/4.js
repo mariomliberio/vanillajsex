4e38910f975edc45c0eff69b1dc686e306f982b1
Mario Liberio <mariomliberio@gmail.com>
2018-03-26 10:15:44 +0200

    wip

--- 4 - Functions/4.js
+++ 4 - Functions/4.js
@@ -1,9 +1,24 @@
 // Functions
 
 // Substraction
-let x = Math.random(Math.floor(1, 100));
-let i = Math.random(Math.floor(1, 100));
-let y = {};
-function resta() {
-  console.log(i - x == y);
-}
+
+let resta = function(x, i) {
+  return x - i;
+};
+console.log("substraction result " + resta(69, 15));
+
+// Division
+
+let division = function(x, i) {
+  return x / i;
+};
+console.log("division result " + division(56, 7));
+
+// Multiplication
+
+let multi = function(x, i) {
+  return x * i;
+};
+console.log("multiplication result " + multi(44, 23));
+
+//
